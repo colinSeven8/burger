3642e0fdaa500859d88793962e6991fec7188f15
colinSeven8 <ccnorse80@gmail.com>
2019-11-19 19:26:52 -0700

    Skeletoned the burger object and started adding to it.

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,20 @@
 
 let orm = require("../config/orm");
 
+// The burger object
+let burger = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    updateOne: () => {
+
+    }
+};
+module.exports = burger;
