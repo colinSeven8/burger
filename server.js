83dde3c8ad733c0a87974125641d9991392720b9
colinSeven8 <ccnorse80@gmail.com>
2020-08-19 15:42:55 -0600

    Doing another commit in order to push to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -15,7 +15,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller.js");
+const routes = require("./controllers/burger_controller.js");
 
 // Routes
 app.use(routes);
