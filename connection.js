a310300bbfcfe537c1a176d37cada26577dd20e6
colinSeven8 <ccnorse80@gmail.com>
2020-08-28 13:29:07 -0600

    changed host to 127.0.0.1

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ if (process.env.CLEARDB_DATABASE_URL) {
   connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     user: "root",
     password: "root",
